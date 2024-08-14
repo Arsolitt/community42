9eea4f3ef0b131f04bc46cb124628b33357c94ff
Arsolitt <arsolitt@gmail.com>
2024-08-14 08:34:18 +0300

    fix: chill and flex profile projects

--- src/shared/assets/projects.ts
+++ src/shared/assets/projects.ts
@@ -47,4 +47,12 @@ export const projects = [
     link: "/project-1",
     collaborators: ["polina-customer-support", "egor-3d-specialist"],
   },
+  {
+    id: 7,
+    name: "Project 1",
+    likes: 10,
+    image: "/images/projects/project-1.png",
+    link: "/project-1",
+    collaborators: ["polina-customer-support", "egor-3d-specialist"],
+  },
 ];
