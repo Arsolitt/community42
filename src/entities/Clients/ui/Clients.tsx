cab1d549dea5ec58ee20ecbfd5b8942c60190444
Arsolitt <arsolitt@gmail.com>
2024-09-07 17:19:17 +0300

    feat: ховер на клиентов

--- src/entities/Clients/ui/Clients.tsx
+++ src/entities/Clients/ui/Clients.tsx
@@ -4,6 +4,7 @@ import { classNames } from "@/features/helpers/className";
 import { Typography } from "@/shared/ui/Typography";
 
 import { clients } from "@/shared/assets/clients";
+import { ArrowIcon } from "@/shared/assets/icons";
 import { AnimatedTitle } from "@/shared/ui/AnimatedTitle/AnimatedTitle";
 import Link from "next/link";
 import cls from "./Clients.module.scss";
@@ -23,8 +24,21 @@ export const Clients = ({ className }: ClientsProps) => {
       <div className={cls.sectionContent}>
         {clients.map((c) => (
           <div className={cls.client} key={c.id}>
+
+            <div className={cls.container}>
+              <Image src={c.image} alt="image" width={108} height={108} className={cls.image} />
+              <div className={cls.overlay} >
+                <Link href={c.link} target="_blank" style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}>
+                  К проекту
+                  <ArrowIcon />
+                </Link>
+              </div>
+            </div>
             <Link href={c.link} target="_blank">
-              <Image src={c.image} alt="image" width={108} height={108} />
               <Typography
                 variant="p"
                 style={{
@@ -36,6 +50,7 @@ export const Clients = ({ className }: ClientsProps) => {
                 {c.name}
               </Typography>
             </Link>
+
           </div>
         ))}
       </div>
