c86abb9c2270d47f3a8faee76ae40211edeb6121
gudkovWay <gudkoviurii@gmail.com>
2024-10-14 13:47:34 +0300

    feature: input like in design

--- src/components/SearchBlock/SearchBlock.tsx
+++ src/components/SearchBlock/SearchBlock.tsx
@@ -2,10 +2,14 @@ import { projects } from '@/shared/assets/projects';
 
 export const SearchBlock = () => {
   return (
+
     <div className='main_search'>
       <div className='container'>
         <div className='flex w-full h-full items-center justify-center text-center'>
-          <input placeholder='поиск работ' className='placeholder:text-[#fff] placeholder:text-[16px] bg-transparent placeholder:opacity-25 caret-[#666666] mr-auto ml-auto' />
+          <input
+            placeholder='поиск работ'
+            className='placeholder:text-[#fff] placeholder:text-[16px] bg-transparent placeholder:opacity-25 caret-[#666666] text-center h-full w-full focus:border-none focus:outline-none border-none appearance-none outline-none'
+          />
         </div>
       </div>
       <div className='container'>
