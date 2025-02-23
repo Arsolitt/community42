24dbf752eda3d6e19726b9c4d2fa5935053e8655
Arsolitt <arsolitt@gmail.com>
2025-02-23 19:38:15 +0300

    refactor: simplify LargeImage component and update responsive sizing

--- src/components/ProjectBlocks/LargeImage/LargeImage.tsx
+++ src/components/ProjectBlocks/LargeImage/LargeImage.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
 
 interface LargeImageProps {
   alt?: string;
@@ -10,46 +7,15 @@ interface LargeImageProps {
 }
 
 export const LargeImage = ({ src, alt = 'image', className }: LargeImageProps) => {
-  const [dimensions, setDimensions] = useState({
-    width: 1180,
-    height: 664
-  });
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 1920) {
-        setDimensions({
-          width: 1836,
-          height: 1032
-        });
-      } else {
-        setDimensions({
-          width: 1180,
-          height: 664
-        });
-      }
-    };
-
-    // Инициализация при монтировании
-    handleResize();
-
-    // Подписка на изменение размера окна
-    window.addEventListener('resize', handleResize);
-
-    // Очистка при размонтировании
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
     <Image
       alt={alt}
       className={className}
-      height={dimensions.height}
+      height={1032}
       quality={100}
+      sizes='(min-width: 1920px) 1836px, 1180px'
       src={src}
-      width={dimensions.width}
+      width={1836}
     />
   );
 };
