4016e3c5e5e92d5cbe7b61c91bd57bc3d803afb5
Arsolitt <arsolitt@gmail.com>
2025-02-23 19:00:06 +0300

    refactor: enhance tag styling and interaction with hover effects

---
+++ src/shared/ui/icons/CrossIcon.tsx
@@ -0,0 +1,17 @@
+export const CrossIcon = () => {
+  return (
+    <svg
+      fill='none'
+      height='12'
+      width='12'
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 12 12'
+    >
+      <path
+        d='M1 1L11 11M1 11L11 1'
+        stroke='#FF66D0'
+        strokeLinecap='square'
+      />
+    </svg>
+  );
+};
