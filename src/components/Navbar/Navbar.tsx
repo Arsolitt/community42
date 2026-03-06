def529e61b0c80923c5ac80525a8b12b27eef42f
Arsolitt <arsolitt@gmail.com>
2026-03-06 14:11:28 +0300

    refactor(navbar): remove unused NavbarDefault and NavbarContacts

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,22 @@
-"use client";
-import { useState } from "react";
+'use client';
 
-import { classNames } from "@features/helpers/className";
+import { classNames } from '@features/helpers/className';
 
-import { NavbarContacts } from "./NavbarContacts/NavbarContacts";
-import { NavbarDefault } from "./NavbarDefault/NavbarDefault";
-import { NavbarSearchButton } from "./SearchButton/SearchButton";
+import { NavbarSearchButton } from './SearchButton/SearchButton';
 
-import cls from "./Navbar.module.css";
+import cls from './Navbar.module.css';
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
-  const [isShown, setIsShown] = useState(false);
   return (
     <nav className={classNames(cls.navbar, {}, [className])}>
-      {!isShown ? (
-        <NavbarDefault className={cls.navbar_wrapper} openContacts={() => setIsShown(true)} />
-      ) : (
-        <NavbarContacts className={cls.navbar_wrapper} closeContacts={() => setIsShown(false)} />
-      )}
+      <div className={cls.navbar_wrapper}>
+        <button className={cls.navbar_button}>Button 1</button>
+        <button className={cls.navbar_button}>Button 2</button>
+      </div>
       <NavbarSearchButton />
     </nav>
   );
