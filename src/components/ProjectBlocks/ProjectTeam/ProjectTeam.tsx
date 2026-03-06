98e545eda35432299db89a9e56b227ceea77bb5a
Arsolitt <arsolitt@gmail.com>
2026-03-06 13:30:19 +0300

    refactor: migrate ProjectTeam from Tailwind to CSS modules

--- src/components/ProjectBlocks/ProjectTeam/ProjectTeam.tsx
+++ src/components/ProjectBlocks/ProjectTeam/ProjectTeam.tsx
@@ -1,12 +1,12 @@
-import Link from "next/link";
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
-import type { ProjectCollaborator } from "@storage/projects";
-import type { TeamMemberWithRole } from "@storage/team/utils";
+import type { ProjectCollaborator } from '@storage/projects';
+import type { TeamMemberWithRole } from '@storage/team/utils';
 
-import { TEAM_PATH } from "@core/config/paths";
+import { TEAM_PATH } from '@core/config/paths';
 
-import styles from "./ProjectTeam.module.css";
+import styles from './ProjectTeam.module.css';
 
 interface ProjectTeamProps {
   collaborators: ProjectCollaborator[];
@@ -20,26 +20,26 @@ interface ProjectMember {
 
 export const ProjectTeam: React.FC<ProjectTeamProps> = ({ collaborators }) => {
   const projectMembers: ProjectMember[] = collaborators.map((collaborator) => {
-    if ("slug" in collaborator) {
+    if ('slug' in collaborator) {
       const teamMember = collaborator as TeamMemberWithRole;
       return {
         name: teamMember.name,
         role: teamMember.role,
-        link: `${TEAM_PATH}/${teamMember.slug}`
+        link: `${TEAM_PATH}/${teamMember.slug}`,
       };
     } else {
       return {
         name: collaborator.name,
         role: collaborator.role,
-        link: collaborator.link
+        link: collaborator.link,
       };
     }
   });
 
   return (
-    <div className='flex flex-wrap gap-24 py-8'>
+    <div className={styles.teamWrapper}>
       {projectMembers.map((projectMember) => (
-        <div key={`${projectMember.link}-${projectMember.role}`} className='flex flex-col'>
+        <div key={`${projectMember.link}-${projectMember.role}`} className={styles.memberWrapper}>
           <h3 className={styles.member_position}>{projectMember.role}</h3>
           <Link href={projectMember.link} className={styles.member_name}>
             {projectMember.name}
