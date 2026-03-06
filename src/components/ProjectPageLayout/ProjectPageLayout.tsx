a2fc0c79b6a52394e666c2089901c2bd075d75da
Arsolitt <arsolitt@gmail.com>
2026-03-06 13:21:33 +0300

    refactor: migrate ProjectPageLayout from Tailwind to CSS modules

--- src/components/ProjectPageLayout/ProjectPageLayout.tsx
+++ src/components/ProjectPageLayout/ProjectPageLayout.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import type { Project } from "@storage/projects";
+import type { Project } from '@storage/projects';
 
-import { ProjectFeed } from "@/components/ProjectBlocks/ProjectFeed";
-import { ProjectFeedback } from "@/components/ProjectBlocks/ProjectFeedback";
-import { ProjectHeader } from "@/components/ProjectBlocks/ProjectHeader";
-import { ProjectTeam } from "@/components/ProjectBlocks/ProjectTeam";
-import { projectLikes } from "@storage/projects";
+import styles from './ProjectPageLayout.module.css';
 
-import { ImgLarge } from "../ProjectBlocks/ImgLarge";
+import { ProjectFeed } from '@/components/ProjectBlocks/ProjectFeed';
+import { ProjectFeedback } from '@/components/ProjectBlocks/ProjectFeedback';
+import { ProjectHeader } from '@/components/ProjectBlocks/ProjectHeader';
+import { ProjectTeam } from '@/components/ProjectBlocks/ProjectTeam';
+import { projectLikes } from '@storage/projects';
+
+import { ImgLarge } from '../ProjectBlocks/ImgLarge';
 
 interface ProjectPageLayoutProps {
   children?: React.ReactNode;
@@ -17,7 +19,7 @@ interface ProjectPageLayoutProps {
 
 export const ProjectPageLayout: React.FC<ProjectPageLayoutProps> = ({ project, children }) => {
   return (
-    <div className='max-w-[1920px] mx-auto px-11'>
+    <div className={styles.wrapper}>
       <ProjectHeader project={project} />
       <ImgLarge alt={project.name} src={project.image} />
       {children}
