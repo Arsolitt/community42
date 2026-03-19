eb9d128b58f08bf0c25657544c36f145c46dc06a
Arsolitt <arsolitt@gmail.com>
2026-03-19 18:23:42 +0300

    feat: add ArrowButton component

---
+++ src/components/ArrowButton/ArrowButton.tsx
@@ -0,0 +1,11 @@
+import { classNames } from "@features/helpers/className";
+
+import cls from "./ArrowButton.module.css";
+
+export interface ArrowButtonProps {
+  className?: string;
+}
+
+export const ArrowButton = ({ className }: ArrowButtonProps) => {
+  return <span className={classNames(cls.arrowButton, {}, [className])} />;
+};
